694eb4e879c751b1eb7817d9b8062c82b5452c42
Paul Batum <paul.batum@gmail.com>
2016-10-04 17:52:23 -0700

    add script for medium clean run

---
+++ clean-run-medium.ps1
@@ -0,0 +1,21 @@
+rm Scenario*\*.txt
+rm Scenario*\*.pdf
+
+pushd Scenario05-Queue-HighCPU-Static-Node
+.\run-medium.bat | tee output.txt
+popd
+pushd Scenario06-Queue-HighCPU-Static-CSharp
+.\run-medium.bat | tee output.txt
+popd
+pushd Scenario07-Queue-HighMemory-Static-Node
+.\run-medium.bat | tee output.txt
+popd
+pushd Scenario08-Queue-HighMemory-Static-CSharp
+.\run-medium.bat | tee output.txt
+popd
+pushd Scenario09-Queue-LowUsage-Static-Node
+.\run-medium.bat | tee output.txt
+popd
+pushd Scenario10-Queue-LowUsage-Static-CSharp
+.\run-medium.bat | tee output.txt
+popd
