2ce2556031bc9c114f1afc7ee173faa6776754e4
Paul Batum <paul.batum@gmail.com>
2016-10-26 17:09:21 -0700

    Add some higher load scenarios

---
+++ Scenario05-Queue-HighCPU-Static-Node/run-high.bat
@@ -0,0 +1,3 @@
+@Echo Scenario05-Queue-HighCPU-Static-Node
+..\bin\SampleUsages.exe QueueTest Queue-NodeJs-CPUIntensive ..\queueitems-static-64.config samples-node-js-cpu-perf-test samples-node-js-cpu-perf-test-output Linear 1000 true 5
+..\bin\SampleUsages.exe QueueTest Queue-NodeJs-CPUIntensive ..\queueitems-static-64.config samples-node-js-cpu-perf-test samples-node-js-cpu-perf-test-output Linear 1000 true 20
---
+++ Scenario06-Queue-HighCPU-Static-CSharp/run-high.bat
@@ -0,0 +1,3 @@
+@Echo Scenario06-Queue-HighCPU-Static-CSharp
+..\bin\SampleUsages.exe QueueTest Queue-CSharp-CPUIntensive ..\queueitems-static-64.config samples-csharp-cpu-perf-test samples-csharp-cpu-perf-test-output Linear 1000 true 5
+..\bin\SampleUsages.exe QueueTest Queue-CSharp-CPUIntensive ..\queueitems-static-64.config samples-csharp-cpu-perf-test samples-csharp-cpu-perf-test-output Linear 1000 true 20
